71e6783ec2f18757b86086f23a0119c2bfe70bfa
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2021-04-01 17:41:21 +0100

    Update

--- config.el
+++ config.el
@@ -120,7 +120,7 @@
 (after! company
   (setq company-idle-delay 0.1
         company-tooltip-limit 10
-        company-minimum-prefix-length 2))
+        company-minimum-prefix-length 1))
 
 ;;
 ;;; Vterm
