1197ea13219d84814289a23d5ef1dd5759eac6ea
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2021-05-28 17:42:23 +0100

    Code formatters

--- +prog.el
+++ +prog.el
@@ -20,22 +20,17 @@
 (remove-hook! (text-mode) #'spell-fu-mode)
 
 (after! sh-script
-  (set-company-backend! 'sh-mode nil)  ; disable backend because of slowliness
-  (set-formatter! 'shfmt "shfmt -i 2 -ci" :modes '(sh-mode)))
+  (set-company-backend! 'sh-mode nil))  ; disable backend because of slowliness
 
 (after! python
-  (setq python-shell-interpreter "/usr/local/opt/python@3.9/bin/python3.9")
-  (set-formatter! 'black "black -q -l 100 -" :modes '(python-mode)))
-
-(setq-hook! 'html-mode-hook +format-with :none)
-(setq-hook! 'web-mode-hook +format-with :none)
-
-(after! (:any js-mode json-mode)
-  (setq js-indent-level 2))
+  (setq python-shell-interpreter "/usr/local/opt/python@3.9/bin/python3.9"))
 
 (after! js2-mode
   (setq-default indent-tabs-mode nil)
   (setq-default js2-basic-offset 2))
+
+(after! (:any js-mode json-mode)
+  (setq js-indent-level 2))
 
 (after! web-mode
   (setq web-mode-indent-style 2
@@ -48,3 +43,17 @@
     '(web-mode-html-attr-name-face :foreground "gray60")
     '(web-mode-html-tag-face :foreground "gray60")
     '(web-mode-html-tag-bracket-face :foreground "gray45")))
+
+;;
+;;; Formatters
+
+(set-formatter! 'shfmt "shfmt -i 2 -ci" :modes '(sh-mode))
+(set-formatter! 'black "black -q -l 100 -" :modes '(python-mode))
+(set-formatter! 'prettier
+  '("prettier"
+    "--print-width" "120"
+    ("--stdin-filepath" "%s" buffer-file-name)
+    :modes '(js2-mode js-mode)))
+
+(setq-hook! 'html-mode-hook +format-with :none)
+(setq-hook! 'web-mode-hook +format-with :none)
