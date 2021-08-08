7492aa77d3e09abf7dee64a7bc367513acc734ec
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2021-08-08 13:06:39 +0100

    Install tubestatus from Melpa

--- packages.el
+++ packages.el
@@ -17,10 +17,7 @@
 (package! lorem-ipsum)
 (package! applescript-mode)
 (package! nginx-mode)
-
-(package! tubestatus :recipe
-  (:host github
-   :repo "smallwat3r/tubestatus.el"))
+(package! tubestatus)
 
 (package! untappd :recipe
   (:host github
